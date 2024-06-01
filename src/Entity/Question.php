bcb9a67bb00bd47310215e98c9ba7267ae9708ee
Kees Kodde <keesje76@hotmail.com>
2024-06-01 16:26:51 +0200

    Add datamodel and migration for it

---
+++ src/Entity/Question.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionRepository::class)]
+class Question
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $label = null;
+
+    #[ORM\Column]
+    private ?int $max_score = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getMaxScore(): ?int
+    {
+        return $this->max_score;
+    }
+
+    public function setMaxScore(int $max_score): static
+    {
+        $this->max_score = $max_score;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Score.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScoreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ScoreRepository::class)]
+class Score
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $student_id = null;
+
+    #[ORM\Column]
+    private ?int $question_id = null;
+
+    #[ORM\Column]
+    private ?int $value = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getStudentId(): ?int
+    {
+        return $this->student_id;
+    }
+
+    public function setStudentId(int $student_id): static
+    {
+        $this->student_id = $student_id;
+
+        return $this;
+    }
+
+    public function getQuestionId(): ?int
+    {
+        return $this->question_id;
+    }
+
+    public function setQuestionId(int $question_id): static
+    {
+        $this->question_id = $question_id;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
