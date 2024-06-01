21ed18d4fee2fe44e3c0247bb9e04d7803f50e31
Kees Kodde <keesje76@hotmail.com>
2024-06-01 19:01:16 +0200

    Add import logic to command class

--- src/Command/ImportScoreSpreadsheetCommand.php
+++ src/Command/ImportScoreSpreadsheetCommand.php
@@ -2,47 +2,101 @@
 
 namespace App\Command;
 
+use App\Entity\Question;
+use App\Entity\Score;
+use App\Entity\Student;
+use App\Repository\ScoreRepository;
+use App\Repository\StudentRepository;
+use App\Repository\QuestionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 #[AsCommand(
     name: 'import:score-spreadsheet',
-    description: 'Add a short description for your command',
+    description: 'Import Excel file with scores from /data/Assignment.xlsx',
 )]
 class ImportScoreSpreadsheetCommand extends Command
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
-    protected function configure(): void
+    private const EXCEL_FILE = '/data/Assignment.xlsx';
+
+    private KernelInterface $appKernel;
+    private QuestionRepository $questionRepository;
+    private StudentRepository $studentRepository;
+    private ScoreRepository $scoreRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(
+        KernelInterface $appKernel,
+        QuestionRepository $questionRepository,
+        StudentRepository $studentRepository,
+        ScoreRepository $scoreRepository,
+        EntityManagerInterface $entityManager,
+    )
     {
-        $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
-        ;
+        $this->appKernel = $appKernel;
+        $this->questionRepository = $questionRepository;
+        $this->studentRepository = $studentRepository;
+        $this->scoreRepository = $scoreRepository;
+        $this->entityManager = $entityManager;
+        parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $io->note($this->appKernel->getProjectDir());
+        $spreadsheet = IOFactory::load($this->appKernel->getProjectDir() . $this::EXCEL_FILE);
+        $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true); // here, the read data is turned into an array
+        $questionRow = array_shift($sheetData);
+        $maxScoreRow = array_shift($sheetData);
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        // Remove label cells.
+        array_shift($questionRow);
+        array_shift($maxScoreRow);
+
+        $questionQueryBuilder = $this->questionRepository->createQueryBuilder('q');
+        $studentQueryBuilder = $this->studentRepository->createQueryBuilder('st');
+        $scoreQueryBuilder = $this->scoreRepository->createQueryBuilder('sc');
+
+        //Clear existing data before import.
+        $questionQueryBuilder->delete()->getQuery()->execute();
+        $studentQueryBuilder->delete()->getQuery()->execute();
+        $scoreQueryBuilder->delete()->getQuery()->execute();
+
+        $questionIds = [];
+        foreach ($questionRow as $colKey => &$questionItem) {
+            $questionEntity = new Question();
+            $questionEntity->setLabel($questionItem);
+            $questionEntity->setMaxScore($maxScoreRow[$colKey]);
+            $this->entityManager->persist($questionEntity);
+            $this->entityManager->flush();
+            $questionIds[$colKey] = $questionEntity->getId();
         }
 
-        if ($input->getOption('option1')) {
-            // ...
+        foreach ($sheetData as $rowNumber => $rowData) {
+            $studentEntity = new Student();
+            $studentEntity->setLabel(array_shift($rowData));
+            $this->entityManager->persist($studentEntity);
+            $this->entityManager->flush();
+            $studentId = $studentEntity->getId();
+            foreach ($rowData as $colKey => $studentScore) {
+                $scoreEntity = new Score();
+                $scoreEntity->setValue($studentScore);
+                $scoreEntity->setStudentId($studentId);
+                $scoreEntity->setQuestionId($questionIds[$colKey]);
+                $this->entityManager->persist($scoreEntity);
+            }
+            $this->entityManager->flush();
         }
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success('Excel file successfully imported.');
 
         return Command::SUCCESS;
     }
